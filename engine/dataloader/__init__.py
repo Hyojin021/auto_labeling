09e784827fe1571f5b179c24afd1586c78f72025
jsk1107 <seungkyu.korea@gmail.com>
2020-12-09 21:14:57 +0900

    adj num_class

--- engine/dataloader/__init__.py
+++ engine/dataloader/__init__.py
@@ -7,6 +7,7 @@ def get_dataloader(config, img_dir, coco_dir):
     train_set = coco.CocoDataset(img_dir, coco_dir, set_name='train', transform=transform_tr())
     val_set = coco.CocoDataset(img_dir, coco_dir, set_name='val', transform=transform_val())
 
+    num_class = len(train_set.load_classes())
     n_train_img = train_set.__len__()
     n_val_img = val_set.__len__()
 
@@ -17,4 +18,4 @@ def get_dataloader(config, img_dir, coco_dir):
         sampler_val = AspectRatioBasedSampler(val_set, batch_size=1, drop_last=False)
         val_loader = DataLoader(val_set, num_workers=config.num_worker, collate_fn=collater, batch_sampler=sampler_val, pin_memory=True)
 
-    return train_loader, n_train_img, val_set, val_loader, n_val_img, train_set.num_classes()
+    return train_loader, n_train_img, val_set, val_loader, n_val_img, num_class
