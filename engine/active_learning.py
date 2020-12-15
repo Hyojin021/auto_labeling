df08f29e330ad2c52efd522e971db94b353a9f57
jsk1107 <seungkyu.korea@gmail.com>
2020-12-15 19:03:50 +0900

    adj img path

--- engine/active_learning.py
+++ engine/active_learning.py
@@ -5,7 +5,8 @@ import torch
 from engine.utils.control_xml import create_label_map
 from engine.utils.voc2coco import convert_annot
 from engine.cfg.config import Config
-
+import glob
+import time
 
 class ActiveLearning(object):
     def __init__(self):
@@ -24,6 +25,7 @@ class ActiveLearning(object):
         # voc2coco 만들기
         # label_map을 사전에 ui에서 생성할 수 있도록 해야함.
         coco_dir = os.path.join(os.path.dirname(xml_dir), 'annotations')
+        print(f'COCO PATH {coco_dir}')
         convert_annot(xml_dir, self.config.label_map_path, coco_dir)
 
         # 학습부 (추후 Active Learning할때 trainer.validation에서 mAP 추출해야함)
@@ -45,10 +47,10 @@ class ActiveLearning(object):
 
         # 추론부
         # 1. 사용자가 Labeling했던 data는 제외시키기(덮어쓰기가 되면 안되기 때문)
+
         ann_name = [os.path.splitext(f.name)[0] for f in os.scandir(xml_dir)]
-        img_name = [os.path.splitext(f.name)[0] for f in os.scandir(img_dir)]
-        img_name = list(set(img_name).difference(set(ann_name)))
-        img_paths = [img_dir + f'/{i}.bmp' for i in img_name]
+        img_name_dict = {os.path.splitext(f.name)[0]: os.path.splitext(f.name)[1] for f in os.scandir(img_dir)}
+        img_names = list(set(img_name_dict.keys()).difference(set(ann_name)))
 
         # 2. Best Model Checkpoint 로드하기
         model_path = f'./engine/run/{self.config.projectname}/best_f1_score_model.pth.tar'
@@ -60,13 +62,19 @@ class ActiveLearning(object):
         # 3. 모델에 이미지 하나씩 넣어서 추론하고, entropy 구하기
 
         all_entropy = {}
-        for i, img_path in enumerate(img_paths):
-            progress = (i + 1) / len(img_paths) * 100
+        total_img_cnt = len(img_names)
+        for i, img_name in enumerate(img_names):
+            progress = (i + 1) / total_img_cnt * 100
+
+            ext = img_name_dict[img_name]
+            img_path = os.path.join(img_dir, img_name + ext)
+
             entropy = detect_image(img_path, model, self.config.label_map_path, al=True)
             all_entropy[img_path] = entropy
-            signals.progress.emit(progress, f'Inferencing image : {i} / {len(img_paths)} complete')
+            signals.progress.emit(progress, f'Inferencing image : {i} / {total_img_cnt} complete')
+
+            if i+1 == int(total_img_cnt * 0.1) or i+1 == 1000:
+                break;
         all_entropy = sorted(all_entropy.items(), key=lambda x: x[1], reverse=True)
         img_list = list(dict(all_entropy[:100]).keys())
         signals.unconfirmed.emit(img_list)
-
-
