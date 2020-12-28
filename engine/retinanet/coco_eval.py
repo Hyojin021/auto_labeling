7ced3e8495a23bbbc79bb1d44c6bbf0fc10a52c7
jsk1107 <seungkyu.korea@gmail.com>
2020-12-28 16:35:57 +0900

    model output 수정

--- engine/retinanet/coco_eval.py
+++ engine/retinanet/coco_eval.py
@@ -21,19 +21,19 @@ def evaluate_coco(dataset, model, json_path, threshold=0.05):
             scale = data['scale']
 
             # run network
-            nms_scores, boxes = model(data['img'].permute(2, 0, 1).cuda().float().unsqueeze(dim=0))
+            # nms_scores, boxes = model(data['img'].permute(2, 0, 1).cuda().float().unsqueeze(dim=0))
+            scores, labels, boxes = model(data['img'].permute(2, 0, 1).cuda().float().unsqueeze(dim=0))
+            # print('nms_scores', scores)
+
+            # correct boxes for image scale
+            boxes /= scale
 
             if boxes.shape[0] > 0:
-
-                scores, labels = nms_scores.max(dim=1)
+                # scores, labels = nms_scores.max(dim=1)
 
                 scores = scores.cpu()
                 labels = labels.cpu()
                 boxes = boxes.cpu()
-
-                # correct boxes for image scale
-                boxes /= scale
-
                 # change to (x, y, w, h) (MS COCO standard)
                 boxes[:, 2] -= boxes[:, 0]
                 boxes[:, 3] -= boxes[:, 1]
