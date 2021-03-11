981a01745c1169ec45bfd6f8430f6f361a1c89a1
jsk1107 <seungkyu.korea@gmail.com>
2021-03-12 03:26:23 +0900

    update config

--- engine/cfg/config.py
+++ engine/cfg/config.py
@@ -7,9 +7,9 @@ class Config(object):
     tensorboard = False
 
     # Config DataLoader
-    batch_size = 5
+    batch_size = 6
     start_epoch = 0
-    epoch = 1
+    epoch = 10
     num_worker = 4
     pin_memory = True
 
